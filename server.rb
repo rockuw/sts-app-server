7ac7be3899c6500fe3646e7be2fd1ff6eb2e3854
Tianlong Wu <tianlong.wtl@alibaba-inc.com>
2015-12-17 11:47:52 +0800

    init commit

---
+++ server.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'aliyun/sts'
+
+CONFIG = File.expand_path('../', __FILE__) + '/config.json'
+
+# A raw policy that defines the policy rules by raw json
+class RawPolicy
+  def initialize(content)
+    @content = content
+  end
+
+  def serialize
+    @content
+  end
+end
+
+get '/' do
+  conf = JSON.load(File.read(CONFIG))
+  sts = Aliyun::STS::Client.new(
+    access_key_id: conf['AccessKeyID'],
+    access_key_secret: conf['AccessKeySecret']
+  )
+
+  policy_file = conf['PolicyFile']
+  if policy_file
+    path = File.expand_path(policy_file)
+    policy = RawPolicy.new(File.read(path)) if File.exist?(path)
+  end
+
+  token = sts.assume_role(
+    conf['RoleArn'], 'my-app', policy, conf['TokenExpireTime'])
+
+  body(
+    {
+      'status' => 200,
+      'AccessKeyId' => token.access_key_id,
+      'AccessKeySecret' => token.access_key_secret,
+      'Security' => token.security_token,
+      'Expiration' => token.expiration
+    }.to_json)
+end
